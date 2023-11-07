2f9fdb046d847e3e5ef2721991fb8d6905f90465
Krishna <skkonda39@gmail.com>
2023-11-07 23:21:35 +0530

    added new experience

--- components/Experience/Experience.Data.ts
+++ components/Experience/Experience.Data.ts
@@ -1,8 +1,42 @@
 const data = [
   {
+    alt: "ftl",
+    url: "/experience/ftl.png",
+    designation: "Full Stack Engineer",
+    company: "Full Throttle Labs",
+    skills: [
+      {
+        alt: "django",
+        url: "/skills/django.png",
+      },
+      {
+        alt: "react",
+        url: "/skills/react.png",
+      },
+      {
+        alt: "python",
+        url: "/skills/python.png",
+      },
+      {
+        alt: "javascript",
+        url: "/skills/javascript.png",
+      },
+      {
+        alt: "typescript",
+        url: "/skills/typescript.png",
+      },
+    ],
+    from: "2023",
+    to: "Present",
+    summary: [
+      "Developed web applications with a focus on both front-end and back-end technologies, ensuring a seamless user experience.",
+      "Managed application performance, security, and scalability while continuously enhancing features through regular updates and deployments."
+    ],
+  },
+  {
     alt: "tcs",
     url: "/experience/tcs.jpg",
-    designation: "Associate System Engineer",
+    designation: "System Engineer",
     company: "TCS",
     skills: [
       {
@@ -19,7 +53,7 @@ const data = [
       },
     ],
     from: "2021",
-    to: "present",
+    to: "2023",
     summary: [
       "Developed automation scripts with modern languages and frameworks. Generated process models illustrating automation engineering progress, specifications and details.",
       "Reliable E2E automation testing with CI/ CD. Trained developers and quality assurance team members in automation programs and processes.",
@@ -79,37 +113,6 @@ const data = [
       "Developed efficient and maintainable software according to business objectives and needs of clients.",
       "Reviewed code, debugged problems and corrected issues. Built APIs and data clients to consume APIs. Developed modern user interfaces.",
       "Identified objectives of clients by analyzing feedback, observing consumers and collecting surveys.",
-    ],
-  },
-  {
-    alt: "student",
-    url: "/experience/student.jpg",
-    designation: "Student",
-    company: "MRIET",
-    skills: [
-      {
-        alt: "git",
-        url: "/skills/git.png",
-      },
-      {
-        alt: "python",
-        url: "/skills/python.png",
-      },
-      {
-        alt: "javascript",
-        url: "/skills/javascript.png",
-      },
-      {
-        alt: "postgresql",
-        url: "/skills/postgresql.png",
-      },
-    ],
-    from: "2016",
-    to: "2020",
-    summary: [
-      "Front-End Development",
-      "Back-End Development",
-      "Responsive Design",
     ],
   },
 ];
